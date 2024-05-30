5c1c702d146779895ceee034dbc3715d81d86c5b
Gulyás Zsolt Máté <85568371+Tetanki-HUN@users.noreply.github.com>
2024-05-30 20:26:20 +0200

    More register cucc

--- Regist/regist.js
+++ Regist/regist.js
@@ -1,8 +1,8 @@
-
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword} from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-
+// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCFjj2TpErZVY8UAW8vYudUSivqVxy0wKo",
     authDomain: "gatheringalaxy.firebaseapp.com",
@@ -12,32 +12,28 @@ const firebaseConfig = {
     appId: "1:639862837184:web:b56be4ef49e3dbbcef4225"
 };
 
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth();
-const db = firebase.firestore(app);
-
-const title = document.getElementById('username').value.trim();
-const author = document.getElementById('email').value.trim();
-const published = document.getElementById('password').value.trim();
-
-let query = db.collection("felhasznalok");
+const auth = getAuth(app);
 
 let regist = document.getElementById("regist");
 
 regist.addEventListener("click", (e) => {
-    let username = document.getElementById("username").value;
+    e.preventDefault(); 
+
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
 
-    createUserWithEmailAndPassword(auth, username, email, password).then((userCredentials) => {
-        alert("Felhasználó sikeres regisztrálva");
-        const user = userCredentials.user;
-        console.log(user);
-    })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-    })
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredentials) => {
+            alert("Sikeres felhasználó registrálás");
+            const user = userCredentials.user;
+            console.log(user);
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.error(`Error [${errorCode}]: ${errorMessage}`);
+            alert(`Hiba: ${errorMessage} (Hibakód: ${errorCode})`);
+        });
 });
-
